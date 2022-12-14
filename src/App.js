73e3e7b8bef80265fedfd4ab695b9ed58d45b021
hamda <hamda.baashe44@gmail.com>
2022-12-14 16:14:04 +0000

    completed bmi-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import {useState} from 'react'
 
 function App() {
+  const [weight,setWeight] = useState(0);
+   const [height,setHeight] = useState(0);
+   const [bmi,setBMI] = useState(0);
+   
+function calc(){
+  
+  setBMI(Math.round(weight/(height*height)));
+  
+}
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='main'>
+    <div className="container">
+      <h1>BMI Calculator</h1>
+     <div className="inp"> <input type='text'  className="weight" onChange={(e)=>{
+       setWeight(+e.target.value);
+     }}    placeholder="Enter your Weight"/>
+      <input type='text' className="height"  onChange={(e)=>{
+       setHeight(+e.target.value);}} placeholder="Enter your Height"/>
+       <button onClick={calc}>Calculate</button>
+      <h3>Your BMI is</h3>
+      <div className="ans"><h3>{bmi}</h3></div>
+      </div>
+    </div>
     </div>
   );
 }
